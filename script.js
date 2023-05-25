086350cd3e286ba95ec6c176d2fe8830bb70ee7e
junionob <junio.waldomiro@gmail.com>
2023-05-25 17:02:11 -0300

    versao 2

--- script.js
+++ script.js
@@ -1,40 +1,42 @@
+const racas = document.getElementById("racas");
+
 
 function recebeFoto(){
-
-    var racas = document.getElementById("racas");
     var racaSelecionada = racas.value; 
+    var mensagemUrl; 
 
-    var urlAPI = `https://dog.ceo/api/breed/${racaSelecionada}/images/random`;
+    urlAPI = `https://dog.ceo/api/breed/${racaSelecionada}/images/random/6`;
 
     fetch (urlAPI)
     .then(function(resposta){
         return resposta.json();
     })
     .then(function(data){
-        mostraImagem(data.message)
+        mostraImagem1(data.message[0])
+        mostraImagem2(data.message[1])
+        mostraImagem3(data.message[2])
+        mostraImagem4(data.message[3])
+        mostraImagem5(data.message[4])
+    
+
     })
     .catch(function(error){
-        console.log("Retornou um erro" + erro);
+        console.log("Retornou um erro" + error);
     });
-
-    
 }
 
-function mostraImagem(imagem_url){
-    document.getElementById('imagem').src = imagem_url;
+function mostraImagem1(imagem_url){
+        document.getElementById('imagem1').src = imagem_url;
 }
-
-
-
-
-//var imgElemento = document.getElementById('imagemAPI');
-
-//imgElemento.src = urlAPI;
-
-/*async function buscaRacas(){
-
-    try {
-        var consultaRacas = await fetch(`https://dog.ceo/api/breed/Pug/images/random`);
-        var consultaInvertida = await consultaRacas.json()
-    }
-}*/
+function mostraImagem2(imagem_url){
+    document.getElementById('imagem2').src = imagem_url;
+}
+function mostraImagem3(imagem_url){
+    document.getElementById('imagem3').src = imagem_url;
+}
+function mostraImagem4(imagem_url){
+    document.getElementById('imagem4').src = imagem_url;
+}
+function mostraImagem5(imagem_url){
+    document.getElementById('imagem5').src = imagem_url;
+}
