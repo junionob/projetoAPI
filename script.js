c180263b79497f0dd25932db85ccffeb5a0ca447
junionob <junio.waldomiro@gmail.com>
2023-05-26 12:12:11 -0300

    novo estilo

--- script.js
+++ script.js
@@ -12,31 +12,16 @@ function recebeFoto(){
         return resposta.json();
     })
     .then(function(data){
-        mostraImagem1(data.message[0])
-        mostraImagem2(data.message[1])
-        mostraImagem3(data.message[2])
-        mostraImagem4(data.message[3])
-        mostraImagem5(data.message[4])
-    
-
+        for(let i = 1; i <= 6; i++){
+                mostraImagem(data.message[i - 1], i)
+        }
     })
     .catch(function(error){
         console.log("Retornou um erro" + error);
     });
+
 }
 
-function mostraImagem1(imagem_url){
-        document.getElementById('imagem1').src = imagem_url;
-}
-function mostraImagem2(imagem_url){
-    document.getElementById('imagem2').src = imagem_url;
-}
-function mostraImagem3(imagem_url){
-    document.getElementById('imagem3').src = imagem_url;
-}
-function mostraImagem4(imagem_url){
-    document.getElementById('imagem4').src = imagem_url;
-}
-function mostraImagem5(imagem_url){
-    document.getElementById('imagem5').src = imagem_url;
+function mostraImagem(imagem_url, i){
+    document.getElementById('imagem' + i).src = imagem_url;
 }
