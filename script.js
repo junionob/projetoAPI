f99c5055e17091ad7db653dea1ac0ae648c16d57
junionob <junio.waldomiro@gmail.com>
2023-05-29 16:51:04 -0300

    versao 10

--- script.js
+++ script.js
@@ -1,26 +1,31 @@
 const racas = document.getElementById("racas");
+var imagemSalva = [];
 
 
 function recebeFoto(){
     var racaSelecionada = racas.value; 
 
-    urlAPI = `https://dog.ceo/api/breed/${racaSelecionada}/images/random/6`;
+    urlAPI = `https://dog.ceo/api/breed/${racaSelecionada}/images/random/3`;
 
     fetch (urlAPI)
     .then(function(resposta){
         return resposta.json();
     })
     .then(function(imagem){
-        for(let i = 1; i <= 6; i++){
-                mostraImagem(imagem.message[i - 1], i)
+        for(var i = 1; i < 4; i++){
+            mostraImagem(imagem.message[i - 1], i)
         }
     })
     .catch(function(error){
         console.log("Retornou um erro" + error);
     });
-
 }
 
 function mostraImagem(imagem_url, i){
     document.getElementById('imagem' + i).src = imagem_url;
 }
+
+function adicionarFoto(){
+    
+}
+
