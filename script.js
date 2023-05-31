33e00f8658a48218e011d5f95595d3e9a8433900
Washington Portugal <portugalw@gmail.com>
2023-05-31 16:41:19 -0300

    chega

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
 const racas = document.getElementById("racas");
-var imagemSalva = [];
-
+const listItems = JSON.parse(localStorage.getItem('data')) || [];
 
 function recebeFoto(){
     var racaSelecionada = racas.value; 
@@ -25,26 +24,44 @@ function mostraImagem(imagem_url, i){
     document.getElementById('imagem' + i).src = imagem_url;
 }
 
-function salvarImagem() {
-    for (let i = 1; i < 4; i++) {
-        var imagem_url = document.getElementById('imagem' + i).src;
-        salvarNoLocalStorage(imagem_url, i);       
+function salvarImagem(i) {
+    let itemSalvo = {
+        id: Math.floor(Math.random() * 200000), //cria o id
+        raca: racas.value,
+        imagem: document.getElementById('imagem' + i).src 
     }
+    salvarNoLocalStorage(itemSalvo);       
 }
 
-function salvarNoLocalStorage(imagem_url, i){
-    var chave = `imagem${i}`;
-    localStorage.setItem(chave, imagem_url);
+function salvarNoLocalStorage(itemSalvo){
+    listItems.push(itemSalvo);
+    localStorage.setItem("data" , JSON.stringify(listItems));
+    exibirImagemSalva();
 }
 
 function exibirImagemSalva(){
-    for (let i = 1; i < 4; i++) {
-        var chave = `imagem${i}`;
-        var imagem_url = localStorage.getItem(chave);
-        if(imagem_url){
-            document.getElementById('foto_adicionada' + i).src = imagem_url
-        }
+    let listarFotos = document.getElementById('lista_imagens');
+    listarFotos.innerHTML = "";
+    for (let i = 1; i < listItems.length; i++) {
+        var imagemLocalStorage = listItems[i].imagem;
+        let criaTag = `
+                    <div class="item">
+                        <img src="${imagemLocalStorage}" alt="" class="img_lista">
+                        <button onclick="excluir(${listItems[i].id})" >X</button>
+                    </div>` //parou aqui 
+        listarFotos.innerHTML += criaTag;
     }
+
+}
+
+function excluir(i){
+    let index = listItems.findIndex(dog => dog.id == i);
+    // console.log(index)
+    listItems.splice(index, 1);
+    // console.log(listItems)
+    localStorage.setItem('data', JSON.stringify(listItems));
+    exibirImagemSalva();
+
 }
 
 exibirImagemSalva()
